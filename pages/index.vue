e12b4e50dc5035cdfd1136d45d47533e36fd855e
iainu <iain@engagingnetworks.net>
2021-12-17 12:06:39 +0000

    initial

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+	<h1>{{ welcome }}</h1>
+	<p>Cillum minim magna ullamco aute officia eiusmod. Consequat aute ut tempor ex occaecat laborum consequat labore tempor consectetur. Aliqua ad laborum pariatur cillum magna aute. Quis adipisicing mollit nostrud id. Excepteur amet minim tempor laborum ullamco sint dolor cillum.</p>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+
+	setup() {
+		
+		return {
+			welcome: ref('Welcome!')
+		}
+
+	}
+
+}
+</script>
