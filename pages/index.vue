9db33afb6429be922293c780f64eb08db86bff01
iainu <iain@engagingnetworks.net>
2022-04-07 22:05:35 +0100

    starting up again

--- pages/index.vue
+++ pages/index.vue
@@ -3,18 +3,9 @@
 	<p>Cillum minim magna ullamco aute officia eiusmod. Consequat aute ut tempor ex occaecat laborum consequat labore tempor consectetur. Aliqua ad laborum pariatur cillum magna aute. Quis adipisicing mollit nostrud id. Excepteur amet minim tempor laborum ullamco sint dolor cillum.</p>
 </template>
 
-<script>
-import { ref } from 'vue'
+<script setup lang="ts">
 
-export default {
+	const welcome: string = 'Welcome traveller'
 
-	setup() {
-		
-		return {
-			welcome: ref('Welcome!')
-		}
-
-	}
-
-}
 </script>
+
